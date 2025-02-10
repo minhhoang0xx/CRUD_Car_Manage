75c969e91a04311e7344a922d4b82edc1ac317bc
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2025-02-10 17:21:10 +0700

    Model

---
+++ CRUD_Car_Manage/Model/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUD_Car_Manage.Model
+{
+	public class Car
+	{
+		public int ID { get; set; }
+		public string Bien_So_Xe { get; set; }
+		public string Loai_Xe { get; set; }
+		public DateTime Ngay_Tao { get; set; }
+		public string Trang_Thai { get; set; }
+
+	}
+}
