6479ea5414f1a55fb272fed0cf4a7a45419373b8
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2025-02-11 14:39:29 +0700

    fix gitignore

--- CRUD_Car_Manage/obj/Debug/net8.0/CRUD_Car_Manage.AssemblyInfo.cs
+++ CRUD_Car_Manage/obj/Debug/net8.0/CRUD_Car_Manage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_Car_Manage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3c152da76e011ee4b3ef6155fa6d9553278bef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490ec6b678e2f56797e06c57fc890a8bc651994c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_Car_Manage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD_Car_Manage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
