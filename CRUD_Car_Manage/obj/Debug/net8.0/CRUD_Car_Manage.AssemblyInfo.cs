715ee25faa0a570fdde702f3a96e4624b68e57a0
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2025-02-12 10:54:31 +0700

    nothing

--- CRUD_Car_Manage/obj/Debug/net8.0/CRUD_Car_Manage.AssemblyInfo.cs
+++ CRUD_Car_Manage/obj/Debug/net8.0/CRUD_Car_Manage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_Car_Manage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0ffd317dbbf7cb7f752861a73ebc0fadf17e84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a404b8ae1f7ee59c8e540d266304d3cecb310d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_Car_Manage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD_Car_Manage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
